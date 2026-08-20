6acba125b76225e9acb93316392aa45147d43daf
nox-lykos <vedant4sh@gmail.com>
2026-08-21 04:47:01 +0530

    api updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import {
 } from "recharts";
 import { Html5Qrcode } from "html5-qrcode";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://battrace.onrender.com";
 
 const BATTERIES = [
   "B0005",
